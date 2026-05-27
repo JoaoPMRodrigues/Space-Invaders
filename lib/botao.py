f9dce00d1921ac898a77fb0ab16081e3c942e531
João Paulo (Pop-os) <jp.estudos17@gmail.com>
2026-05-26 23:20:29 -0300

    Remove bug do double click

--- lib/botao.py
+++ lib/botao.py
@@ -3,21 +3,22 @@ from lib.entidade import *
 
 
 class Botao(Entidade):
+    was_pressed = False
+
     def __init__(self, imagem, window, x, y):
         super().__init__(imagem, window, x, y)
-        self.was_pressed = False
 
     def clicked(self, window):
         mouse = window.mouse
         hovering = mouse.is_over_object(self.sprite)
         pressed = mouse.button_pressed(1)
 
-        if hovering and pressed and not self.was_pressed:
-            self.was_pressed = True
+        if hovering and pressed and not Botao.was_pressed:
+            Botao.was_pressed = True
             return True
 
         if not pressed:
-            self.was_pressed = False
+            Botao.was_pressed = False
 
         return False
 
